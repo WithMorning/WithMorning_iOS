33d325e28a46aa7611052a79b7358018402db9cc
Ash_amad_KR <dkstpgns0617@gmail.com>
2024-07-07 00:17:18 +0900

    알림음설정

--- WithMorning_iOS/Scene/MainPage/MakeAlarmViewController.swift
+++ WithMorning_iOS/Scene/MainPage/MakeAlarmViewController.swift
@@ -33,9 +33,7 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
         let scrollview = UIScrollView()
         scrollview.addSubview(contentView)
         scrollview.isScrollEnabled = true
-        
         scrollview.delegate = self
-        
         scrollview.showsVerticalScrollIndicator = false
         return scrollview
     }()
@@ -51,7 +49,7 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 8
-        view.addSubviews(timePicker,bar1,alarmStackView)
+        view.addSubviews(timePicker,bar1,alarmViewStackView)
         return view
     }()
     
@@ -68,14 +66,41 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
         picker.backgroundColor = .clear
         return picker
     }()
-    
-    private lazy var alarmStackView: UIStackView = {
+    //MARK: - 반복 요일 스택뷰
+    private lazy var alarmViewStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .fill
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 8
+        stackView.addSubviews(repeatLabel,repeatDayLabel)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(repeatDay))
+        stackView.addGestureRecognizer(tapGestureRecognizer)
         return stackView
+    }()
+    
+    
+    private lazy var repeatLabel : UILabel = {
+        let label = UILabel()
+        label.text = "반복 요일"
+        label.font = DesignSystemFont.Pretendard_Bold14.value
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var repeatDayLabel : UILabel = {
+        let attributedString1 = NSMutableAttributedString(string: "없음   ")
+        let imageAttachment1 = NSTextAttachment()
+        imageAttachment1.image = UIImage(systemName: "greaterthan")
+        imageAttachment1.bounds = CGRect(x: 0, y: -3, width: 10, height: 16)
+        attributedString1.append(NSAttributedString(attachment: imageAttachment1))
+        
+        let label = UILabel()
+        label.attributedText = attributedString1
+        label.textAlignment = .right
+        label.textColor = DesignSystemColor.Gray400.value
+        label.font = DesignSystemFont.Pretendard_Medium14.value
+        return label
     }()
     
     //MARK: - 알림음
@@ -83,9 +108,56 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 8
-        //        view.addSubviews()
+        view.addSubviews(SoundViewStackView,bar2)
         return view
     }()
+    
+    private lazy var bar2 : UIView = {
+        let view = UIView()
+        view.backgroundColor = DesignSystemColor.Gray200.value
+        return view
+    }()
+    
+//MARK: - 알림음 스택뷰
+    private lazy var SoundViewStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        stackView.backgroundColor = .gray
+        stackView.addSubviews(alarmsoundLabel,alarmsoundLabel2)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(soundsetting))
+        stackView.addGestureRecognizer(tapGestureRecognizer)
+        return stackView
+    }()
+    
+    
+    private lazy var alarmsoundLabel : UILabel = {
+        let label = UILabel()
+        label.text = "알림음"
+        label.font = DesignSystemFont.Pretendard_Bold14.value
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var alarmsoundLabel2 : UILabel = {
+        let attributedString1 = NSMutableAttributedString(string: "기본   ")
+        let imageAttachment1 = NSTextAttachment()
+        imageAttachment1.image = UIImage(systemName: "greaterthan")
+        imageAttachment1.bounds = CGRect(x: 0, y: -3, width: 10, height: 16)
+        attributedString1.append(NSAttributedString(attachment: imageAttachment1))
+        
+        let label = UILabel()
+        label.attributedText = attributedString1
+        label.textAlignment = .right
+        label.textColor = DesignSystemColor.Gray400.value
+        label.font = DesignSystemFont.Pretendard_Medium14.value
+        return label
+    }()
+
+    
     //MARK: - 모임명
     private lazy var groupView : UIView = {
         let view = UIView()
@@ -160,23 +232,48 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
             $0.height.equalTo(208)
             
         }
-        
         timePicker.snp.makeConstraints{
             $0.leading.trailing.equalToSuperview().inset(83)
-            $0.height.equalTo(100)
+            $0.height.equalTo(120)
         }
         bar1.snp.makeConstraints{
-            $0.center.equalToSuperview()
+            $0.top.equalTo(timePicker.snp.bottom).offset(16)
             $0.height.equalTo(1)
             $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        alarmViewStackView.snp.makeConstraints{
+            $0.top.equalTo(bar1.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+        repeatLabel.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+        }
+        repeatDayLabel.snp.makeConstraints {
+            $0.trailing.centerY.equalToSuperview()
         }
         
         soundView.snp.makeConstraints{
             $0.top.equalTo(timerView.snp.bottom).offset(8)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(112)
-            
         }
+        SoundViewStackView.snp.makeConstraints{
+            $0.center.equalToSuperview().multipliedBy(0.5)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        bar2.snp.makeConstraints{
+            $0.center.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        alarmsoundLabel.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+        }
+        alarmsoundLabel2.snp.makeConstraints {
+            $0.trailing.centerY.equalToSuperview()
+        }
+        
         
         groupView.snp.makeConstraints{
             $0.top.equalTo(soundView.snp.bottom).offset(8)
@@ -190,12 +287,12 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(112)
             $0.width.equalTo(alarmScrollVeiw.frameLayoutGuide)
-            $0.bottom.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(30)
         }
         
         saveButton.snp.makeConstraints{
             $0.leading.trailing.bottom.equalToSuperview()
-            $0.height.equalTo(100)
+            $0.height.equalTo(92)
         }
         
     }
@@ -204,6 +301,13 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func repeatDay(){
+        print("요일설정")
+    }
+    
+    @objc func soundsetting(){
+        print("요일설정")
+    }
 }
 
 //Preview code
