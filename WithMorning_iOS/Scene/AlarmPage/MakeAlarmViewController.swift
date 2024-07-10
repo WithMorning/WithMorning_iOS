6a3a33b572268931b193323c16c209a77f3d5715
Ash_amad_KR <dkstpgns0617@gmail.com>
2024-07-10 12:13:54 +0900

    키보드 위아래 움직임 수정

--- WithMorning_iOS/Scene/AlarmPage/MakeAlarmViewController.swift
+++ WithMorning_iOS/Scene/AlarmPage/MakeAlarmViewController.swift
@@ -254,6 +254,7 @@ class MakeAlarmViewController : UIViewController, UIScrollViewDelegate, UISheetP
         self.view.backgroundColor = DesignSystemColor.Gray150.value
         SetUI()
         hideKeyboardWhenTappedAround()
+        setUpKeyboard()
     }
     
     func SetUI(){
@@ -417,7 +418,37 @@ extension MakeAlarmViewController : UITextFieldDelegate {
         textField.layer.borderWidth = 0
         
     }
+    
+    func setUpKeyboard() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 }
+
+
+
+
+
 
 //Preview code
 #if DEBUG
