e75c7f5e81f508836d3d04fa448204eee2c9d718
Ash_amad_KR <dkstpgns0617@gmail.com>
2024-07-25 16:54:19 +0900

    온보딩 제작중

---
+++ WithMorning_iOS/Scene/Onboarding/OnboardingTutorial/TutorialSecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TutorialSecondViewController.swift
+//  WithMorning_iOS
+//
+//  Created by 안세훈 on 7/25/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Alamofire
+
+class TutorialSecondViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+}
