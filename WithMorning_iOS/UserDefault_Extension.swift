2c53d32f619b3375edf6775a5b29f290ac8b9a12
Ash_amad_KR <dkstpgns0617@gmail.com>
2024-09-11 15:59:18 +0900

    유저디폴트 설정

---
+++ WithMorning_iOS/UserDefault_Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefault_Extension.swift
+//  WithMorning_iOS
+//
+//  Created by 안세훈 on 9/11/24.
+//
+
+import Foundation
+import UIKit
+
+open class UserDefaults : NSObject{
+    
+    open class var standard : UserDefaults { get }
+    
+}
